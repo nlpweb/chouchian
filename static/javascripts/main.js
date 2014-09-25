1e75194e217e410a6478688567d79111fc22ea77
maxis <maxis1718@gmail.com>
2014-09-25 17:49:26 +0800

    Support self-defined spreadsheet

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,51 +1,59 @@
 
 // var candidates = ['玫樺0','玫樺1','玫樺2','玫樺3','玫樺4','玫樺5','玫樺6','玫樺7','玫樺8','玫樺9']
+// var gdocurl = 'https://docs.google.com/spreadsheets/d/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/pubhtml'
+// var url = 'https://spreadsheets.google.com/feeds/list/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/od6/public/values?alt=json-in-script&callback=?';
 
-var gdocurl = 'https://docs.google.com/spreadsheets/d/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/pubhtml'
-var url = 'https://spreadsheets.google.com/feeds/list/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/od6/public/values?alt=json-in-script&callback=?';
+var LOCK = true;
 
 var TEXT = $('.button-text');
 var LOADING = $('.fa-spinner');
 
-var current_class = 'classa';
-// my_students = {
-//     classa: [...],
-//     classb: [...],
-// }
-var my_students = {};
+var url = '';
+var all_candidates = {};
+var spread_id = '';
+var sheet_name = '';
 
 $(window).on('hashchange', function(e){
 
+    if(LOCK){ return false; }
+
+    // hash:
+    //  "#spread=1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ&sheet=classa"
     var hash = location.hash;
-    current_class = hash.replace( /^#/, '' );
 
-    console.log(current_class);
+    // param:
+    //  {spread: "1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ", sheet: "classa"}
+    var param = $.deparam.fragment( hash );
+    console.debug('parse hash',hash,'-> param:',param);
 
-    if($.isEmptyObject(my_students))
-    {
-        console.log('>> run fetchALL');
-        fetchALL(url);
-    }else
-    {
-        // rebind event by passing differernt `current_class`
-        bind_events(my_students[current_class]);
+    // extract params
+    spread_id = param.spread;
+    sheet_name = param.sheet;
+
+    // form the url of spreadsheet
+    url = form_url(spread_id);
+
+    if($.isEmptyObject(all_candidates)) {
+        console.debug('fetch data from', url);
+        fetchALL( url );
+    }else {
+        // rebind event by passing differernt name of sheet
+        console.debug('all_candidates has been fetched. bind events');
+        bind_events( all_candidates[sheet_name] );
     }
     
 });
 
-var bind_events = function(candidates)
-{   
-    console.log("====== in bind_events ======");
-    console.log("current_class:",current_class);
-    console.log("my_students:",my_students);
-    console.log("candidates:",candidates);
+var bind_events = function(candidates) {   
+    console.debug("====== bind_events ======");
 
+    console.debug('bind keyup event');
     $(document).keyup(function(e){
-        if(e.which == 13 || e.which == 32)
-        {
+        if(e.which == 13 || e.which == 32) {
             $('#yo-btn').click();
         }
     });
+    console.debug('bind click event');
     $('#yo-btn').click(function(e){
 
         var idx = Math.floor((Math.random()*candidates.length)+1);
@@ -57,24 +65,23 @@ var bind_events = function(candidates)
     });    
 }
 
-function fetchALL(url) 
-{
-    if(TEXT.hasClass("hide"))
-    {
+var form_url = function(spread_id){
+    var spread_url = 'https://spreadsheets.google.com/feeds/list/'+spread_id+'/od6/public/values?alt=json-in-script&callback=?';
+    return spread_url;
+}
+
+function fetchALL(url) {
+
+    // open spinner
+    if(TEXT.hasClass("hide")) {
         LOADING.fadeIn(50);
     }else{
         TEXT.fadeOut(200, function(){ LOADING.fadeIn(50); });    
     }
     
     $.getJSON(url, function(data) {
-        // open spinner
-
-        // TEXT.hide(0, function(){
-            // LOADING.fadeIn(300);
-        // });
 
         var classes_num = -1;
-        // console.log(data);
         $.each(data.feed.entry, function(i,obj){
 
             // "classb: 搗蛋的學生B"
@@ -94,33 +101,30 @@ function fetchALL(url)
                 var cls_name = $.trim(students[sid*2]);
                 var stu_name = $.trim(students[sid*2+1]);
 
-                if(!my_students.hasOwnProperty(cls_name)){
-                    my_students[cls_name] = new Array();
+                if(!all_candidates.hasOwnProperty(cls_name)){
+                    all_candidates[cls_name] = new Array();
                 }
-                my_students[cls_name].push(stu_name);
+                all_candidates[cls_name].push(stu_name);
             }
         });
         
     }).done(function () {
-        // var candidates = my_students[current_class];
-        console.log("====== ajax done ======");
-        console.log("current_class:",current_class);
-        console.log("my_students:",my_students);
-        console.log("candidates:",my_students[current_class]);
-        console.log("run bind_events");
+        // var candidates = all_candidates[current_class];
+        console.debug("====== ajax done ======");
+        console.debug("sheet_name:",sheet_name);
+        console.debug("all_candidates:",all_candidates);
+        console.debug("candidates:",all_candidates[sheet_name]);
+        console.debug("run `bind_events`");
+
+        bind_events(all_candidates[sheet_name]);
 
         // close spinner
         LOADING.fadeOut(200, function(){ TEXT.fadeIn(150); });
-            // TEXT.fadeIn(100);
-        // });
-        bind_events(my_students[current_class]);
-        // console.log(my_students);
     });
 }
 
 $(document).ready(function(){
 
     $(window).trigger('hashchange');
-    // bind_events();
 });
 
