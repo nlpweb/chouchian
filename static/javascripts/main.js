7b24209f1f3f47e7864571dbaac9fb2b07e5fcaa
maxis <maxis1718@gmail.com>
2014-09-26 15:05:06 +0800

    Support any source of google spreadsheet

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -2,11 +2,14 @@
 // var candidates = ['玫樺0','玫樺1','玫樺2','玫樺3','玫樺4','玫樺5','玫樺6','玫樺7','玫樺8','玫樺9']
 // var gdocurl = 'https://docs.google.com/spreadsheets/d/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/pubhtml'
 // var url = 'https://spreadsheets.google.com/feeds/list/1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ/od6/public/values?alt=json-in-script&callback=?';
-
+// https://docs.google.com/spreadsheets/d/1ShT62S65ypu_9D9OFBd6_ImWBpngi-y_iK0wYmw4YLU/pubhtml
 var LOCK = true;
 
 var TEXT = $('.button-text');
 var LOADING = $('.fa-spinner');
+
+// var content = $('.content-wrap');
+// var info = $('.info');
 
 var url = '';
 var all_candidates = {};
@@ -14,8 +17,6 @@ var spread_id = '';
 var sheet_name = '';
 
 $(window).on('hashchange', function(e){
-
-    if(LOCK){ return false; }
 
     // hash:
     //  "#spread=1-teCetf_KoSe2TkI9L8eyp9O7slcr6R3Bd558kPUOcQ&sheet=classa"
@@ -30,19 +31,88 @@ $(window).on('hashchange', function(e){
     spread_id = param.spread;
     sheet_name = param.sheet;
 
-    // form the url of spreadsheet
-    url = form_url(spread_id);
+    $('#spread-url').val(spread_id);
+    $('#sheet-name').val(sheet_name);
 
-    if($.isEmptyObject(all_candidates)) {
-        console.debug('fetch data from', url);
-        fetchALL( url );
+    $('#change-btn').click();
+
+    if(!$.isEmptyObject(spread_id) && !$.isEmptyObject(sheet_name))
+    {
+        console.log('go');
+        
+        $('#info').fadeOut(200, function(){
+            $('#content').fadeIn(100);    
+        });
+
+        url = form_url(spread_id);
+
+        // if(LOCK){ return false; }
+        if($.isEmptyObject(all_candidates)) {
+            console.debug('fetch data from', url);
+            fetchALL( url );
+        }else {
+            // rebind event by passing differernt name of sheet
+            console.debug('all_candidates has been fetched. bind events');
+            bind_events( all_candidates[sheet_name] );
+        }
     }else {
-        // rebind event by passing differernt name of sheet
-        console.debug('all_candidates has been fetched. bind events');
-        bind_events( all_candidates[sheet_name] );
+        // hide Yo!, show info
+        $('#content').fadeOut(0, function(){
+            $('#info').fadeIn(0);    
+        });
+        console.log('enter missing field');
+
     }
     
 });
+
+function extract_spread_id(url){
+    // var url = 'https://docs.google.com/spreadsheets/d/1ShT62S65ypu_9D9OFBd6_ImWBpngi-y_iK0wYmw4YLU/pubhtml';
+    if( url.indexOf('https://') > -1 && url.indexOf('/pubhtml')){
+        var arr = url.split('/pubhtml')[0].split('/');
+        spread_id = arr[arr.length-1];
+        return spread_id;
+    }else if(url.indexOf("/") > -1){
+        return false;
+    }else {
+        return url;
+    }
+    
+}
+
+var bind_input_event = function() {
+
+    $('#change-btn').click(function(e){
+
+        spread_id = extract_spread_id( $.trim($('#spread-url').val()) );
+        $('#spread-url').val(spread_id);
+
+        // get hash value
+        sheet_name = $.trim($('#sheet-name').val());
+
+        // check nessecary fileds
+        var error = false;
+        $.each($('.input-area').find('input'), function(i, obj){
+            if($.trim($(obj).val()).length == 0)
+            {
+                $(obj).addClass('warning');
+                $(obj).focus();
+                error = true;
+                return false;
+            }else{
+                $(obj).removeClass('warning');
+                // return false;
+            }
+        });
+
+        if(!error) {
+            var hash_value = '#spread='+spread_id+'&'+'sheet='+sheet_name;
+            // change hash
+            window.location.hash = hash_value;
+        }
+
+    });
+} 
 
 var bind_events = function(candidates) {   
     console.debug("====== bind_events ======");
@@ -59,8 +129,8 @@ var bind_events = function(candidates) {
         var idx = Math.floor((Math.random()*candidates.length)+1);
         var name = candidates[idx];
 
-        $('.result').hide(100, function(){
-            $('.result').text(name).show(200);
+        $('.result').slideUp(100, function(){
+            $('.result').text(name).slideDown(500);
         });
     });    
 }
@@ -119,12 +189,27 @@ function fetchALL(url) {
         bind_events(all_candidates[sheet_name]);
 
         // close spinner
-        LOADING.fadeOut(200, function(){ TEXT.fadeIn(150); });
+
+        LOADING.fadeOut(200, function(){ TEXT.fadeIn(200); });
     });
+}
+
+function other_events () {
+    $('.input-area').find('input').click(function(){
+        $(this).select();
+    }).keyup(function(e){
+        if(e.which == 13)
+        {
+            $('#change-btn').click();
+        }
+    });
+
 }
 
 $(document).ready(function(){
 
+    other_events();
+    bind_input_event();
     $(window).trigger('hashchange');
 });
 
